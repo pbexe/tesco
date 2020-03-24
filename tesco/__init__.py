4e69e6361eab3690c02f944c20b562034f3cd5fd
Miles Budden <miles@pop-os.localdomain>
2020-03-24 13:01:16 +0000

    Commented

--- tesco/__init__.py
+++ tesco/__init__.py
@@ -1,4 +1,4 @@
 from .main import Tesco
 
 
-__version__ = '0.1.0'
+__version__ = '0.7.0'
--- tesco/main.py
+++ tesco/main.py
@@ -2,10 +2,32 @@ import requests
 from typing import List
 
 class Tesco:
+    """The Tesco wrapper object
+    """
+    
     def __init__(self, key:str) -> None:
+        """init
+        
+        Args:
+            key (str): The API key
+        """
+        
         self.key = key
     
     def grocery_search(self, query:str, offset:int=0, limit:int=10) -> List:
+        """Searches grocery items stocked by Tescolabs
+        
+        ```
+        Args:
+            query (str): The search query
+            offset (int, optional): The offset used for pagination. Defaults to 0.
+            limit (int, optional): The number of results to return. Defaults to 10.
+        
+        Returns:
+            List: List of products
+        ```
+        """
+        
         params:dict = {
             "query": query,
             "offset": offset,
@@ -21,6 +43,20 @@ class Tesco:
         return r.json()["uk"]["ghs"]["products"]["results"]
     
     def product_data(self, gtin:str="", tpnb:str="", tpnc:str="", catid:str="") -> List:
+        """Gets information about a product
+        
+        ```
+        Args:
+            gtin (str, optional): The gtin of the product. Defaults to "".
+            tpnb (str, optional): The tpnb of the product. Defaults to "".
+            tpnc (str, optional): The tpnc of the product. Defaults to "".
+            catid (str, optional): The catid of the product. Defaults to "".
+        
+        Returns:
+            List: The information about the product
+        ```
+        """
+        
         params:dict = {
             "gtin": gtin,
             "tpnb": tpnb,
