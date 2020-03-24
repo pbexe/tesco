aae856a94ac037fb214d35fde36f2a8970852fc3
Miles Budden <miles@pop-os.localdomain>
2020-03-24 00:29:25 +0000

    Made the interface less stupid

--- tesco/main.py
+++ tesco/main.py
@@ -3,21 +3,19 @@ from typing import List
 
 class Tesco:
     def __init__(self, key:str) -> None:
-        print("Init tesco")
         self.key = key
     
     def grocery_search(self, query:str, offset:int=0, limit:int=10) -> List:
-        params = {
+        params:dict = {
             "query": query,
             "offset": offset,
             "limit": limit           
         }
-        headers = {
+        headers:dict = {
             "Ocp-Apim-Subscription-Key": self.key
         }
-        base = "https://dev.tescolabs.com/grocery/products/"
+        base:str = "https://dev.tescolabs.com/grocery/products/"
         
         r = requests.get(base, params=params, headers=headers)
         
-        for item in r.json()["uk"]["ghs"]["products"]["results"]:
-            yield item["name"]
+        return r.json()["uk"]["ghs"]["products"]["results"]
